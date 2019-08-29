0e63b97f39ff125354ece6ac0f14a00c5fd5c5df
Mihai Babet <mihai.babet@metrosystems.net>
2019-08-29 13:14:28 +0300

    use aync await

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 
 const fs = require('fs')
+const util = require('util')
 const path = require('path')
 const express = require('express')
 const compression = require('compression')
 const cors = require('cors')
 
-const { PORT = 8000 } = process.env
-const html = fs.readFileSync(
-  path.resolve(__dirname, './static/index.html'),
-  'utf8'
-)
+const readFile = util.promisify(fs.readFile)
 
 const app = express()
 app.use(compression())
-app.use(express.static(path.join(__dirname, './static')))
+app.use(express.static(path.join(__dirname, './build')))
 app.use(cors({
   origin: true,
   methods: ['GET', 'POST']
 }))
-app.get('/', (req, res) => {
+app.get('/', async (_, res) => {
+  const html = await readFile(path.resolve(__dirname, './build/index.html'), 'utf8')
   res.set('Content-Type', 'text/html')
   res.send(html)
   res.end()
 })
+
+const { PORT = 8000 } = process.env
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`) // eslint-disable-line no-console
 })
